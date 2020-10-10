f6ffb1d4b31327c1649693b132caa8d062f1a83a
franklinharvey <franklinadamsharvey@gmail.com>
2020-10-09 19:16:21 -0600

    get connect by url for heroku

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -8,12 +8,8 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 			imports: [ConfigModule],
 			inject: [ConfigService],
 			useFactory: (configService: ConfigService) => ({
+				url: configService.get('DATABASE_URL'),
 				type: 'postgres',
-				host: configService.get('POSTGRES_HOST'),
-				port: configService.get('POSTGRES_PORT'),
-				username: configService.get('POSTGRES_USER'),
-				password: configService.get('POSTGRES_PASSWORD'),
-				database: configService.get('POSTGRES_DB'),
 				entities: [__dirname + '/../**/*.entity{.ts,.js}'],
 				synchronize: false,
 			}),
