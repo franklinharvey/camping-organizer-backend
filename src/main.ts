b4d2e8cccd7b3fd4a12aa0b86391b639baac1097
Franklin Harvey <fharvey@gloo.us>
2020-11-08 13:08:50 -0700

    cert stuff

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,32 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+// import { NestFactory } from '@nestjs/core';
+// import { readFileSync } from 'fs';
+// import { AppModule } from './app.module';
+import express from 'express';
 
-async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
-	app.enableCors();
-	await app.listen(process.env.PORT || 3001);
-}
-bootstrap();
+const app = express();
+app.get(
+	'/.well-known/acme-challenge/fhPKBIOC-DNcS08rQgHkMrGpCo9o5Q5UA18J7wSFcI0',
+	(_, res) => {
+		res.send(
+			`fhPKBIOC-DNcS08rQgHkMrGpCo9o5Q5UA18J7wSFcI0.EMbtsog1IQM2BxiD8LbPxXgKegEQn8WlYpiaWbWrPHA`,
+		);
+	},
+);
+
+app.listen(process.env.PORT);
+
+// async function bootstrap() {
+// 	let httpsOptions = {
+// 		key: readFileSync(__dirname + '/../ssl/server.key'),
+// 		cert: readFileSync(__dirname + '/../ssl/crt.crt'),
+// 	};
+// 	const app = await NestFactory.create(AppModule, { httpsOptions });
+// 	const expressInstance: express.Express = express();
+// 	expressInstance.get('/.well-known/acme-challenge/:id', function(req, res) {
+// 		res.send(req.params.id + '.' + httpsOptions.cert.toString());
+// 	});
+
+// 	app.enableCors();
+// 	await app.listen(process.env.PORT);
+// }
+// bootstrap();
